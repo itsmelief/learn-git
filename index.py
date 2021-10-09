1522bfc01fb9b18da8edd9dfa5d28657934b221a
TOSHIBA <alieframadhan150@gmail.com>
2021-10-09 09:15:41 +0700

    first commit

---
+++ index.py
@@ -0,0 +1,8 @@
+print('''  
+		[ Saya akan menebak umur Anda ]
+	[ Silahkan Anda masukkan tahun kelahiran Anda ]
+	''')
+
+tahun_kelahiran = input('  Masukkan tahun kelahiran : ')
+tahun_sekarang = 2021 - int(tahun_kelahiran)
+print('  Umur Anda ' + str(tahun_sekarang) + ' tahun')
